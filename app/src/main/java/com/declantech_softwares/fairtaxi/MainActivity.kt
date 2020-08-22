3c553a7eee56fca9806baeadf379969c7e24bbea
zedonboy <zedonbiz@gmail.com>
2020-08-22 22:33:57 +0100

    My first Commit

---
+++ app/src/main/java/com/declantech_softwares/fairtaxi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.declantech_softwares.fairtaxi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.declantech_softwares.fairtaxi.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
---
+++ app/src/main/java/com/declantech_softwares/fairtaxi/ShowTaxiFragment.kt
@@ -0,0 +1,24 @@
+package com.declantech_softwares.fairtaxi
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ShowTaxiFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_show_taxi, container, false)
+    }
+
+
+}
